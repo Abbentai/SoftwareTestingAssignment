591314ae806e3e853d77b1da29ed49bc0f11644d
Abbentai <themcpebird@gmail.com>
2024-03-08 16:38:01 +0100

    ding

---
+++ src/main/java/org/dingus/FileReading.java
@@ -0,0 +1,18 @@
+package org.dingus;
+
+import 
+
+public class FileReading {
+    try {
+        File myObj = new File("filename.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
+}
---
+++ src/main/java/org/dingus/Main.java
@@ -0,0 +1,8 @@
+package org.dingus;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
